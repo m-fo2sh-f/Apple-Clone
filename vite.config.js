80e2e4268b15d711864863610359441b1e16848e
Mohamed Fouad <mohamedabc336@gmail.com>
2025-10-31 20:37:26 +0200

    Deploy project to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-    base: '/Apple-Clone/',
+    plugins: [react(), tailwindcss()],
+    base: '/Apple-Clone/',  // تأكد من أن هذا يتطابق مع اسم المستودع الخاص بك
 })
